d170afb6fe8ecea86269b375c26682f554d2544e
sigee <isolomongee@yahoo.com>
2016-12-29 17:45:01 -0600

    Carousel

--- js/init.js
+++ js/init.js
@@ -9,5 +9,6 @@
       });
     $('.parallax').parallax();
 
+    $('.carousel.carousel-slider').carousel({ full_width: true });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
